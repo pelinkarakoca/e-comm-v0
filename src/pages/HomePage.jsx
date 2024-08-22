a76b552de3f75c5fa6dd5370dd93f7f4ff2df77b
Pelin Karakoca <karakocapelin@gmail.com>
2024-08-22 20:09:22 +0300

    Delete duplicate files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+
+export default function HomePage() {
+  return (
+    <div className=" border-customWedgewood border-2">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      customDarkBlue: "rgb(var(--custom-big-stone))",
+      customBluewood: "rgb(var(--custom-bluewood))",
+      customOrient: "rgb(var(--custom-orient))",
+      customWedgewood: "rgb(var(--custom-wedgewood))",
+      customEucalyptus: "rgb(var(--custom-eucalyptus))",
+      customKeppel: "rgb(var(--custom-keppel))",
+      customTallPoppy: "rgb(var(--custom-tall-poppy))",
+      customAmaranth: "rgb(var(--custom-amaranth))",
+    },
     extend: {},
   },
   plugins: [],
