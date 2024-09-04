960a8521590a72bbe784220e92b0f123d0d504dd
Pelin Karakoca <karakocapelin@gmail.com>
2024-09-04 15:52:27 +0300

    Build: embla library

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,11 @@
-import ProductCard from "../components/ProductCard";
 import Header from "../layout/Header";
-
+import Carousel from "../components/Carousel";
 // import Footer from "../layout/Footer";
 export default function HomePage() {
   return (
     <div>
       <Header />
-      <ProductCard />
+      <Carousel />
     </div>
   );
 }
